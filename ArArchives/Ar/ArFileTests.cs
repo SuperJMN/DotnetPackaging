bdb921b063c3b2d9567c9445362c0a2d2f9f6be8
José Manuel Nieto <superjmn@outlook.com>
2023-11-01 00:51:38 +0100

    AR With Tar inside works fine

--- ArArchives/Ar/ArFileTests.cs
+++ ArArchives/Ar/ArFileTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reactive.Linq;
 using Archiver;
 using Archiver.Ar;
+using Archiver.Tar;
 using Zafiro.IO;
 using EntryData = Archiver.Ar.EntryData;
 using Properties = Archiver.Ar.Properties;
@@ -10,7 +11,7 @@ namespace Archive.Tests.Ar;
 public class ArFileTests
 {
     [Fact]
-    public async Task Write()
+    public async Task Regular_file()
     {
         var contents ="""
                       2.0
@@ -34,5 +35,51 @@ public class ArFileTests
 
         await using var output = File.Create("C:\\Users\\JMN\\Desktop\\Actual.ar");
         await ar.Bytes.DumpTo(output);
+    }
+
+    [Fact]
+    public async Task Ar_with_tar_inside()
+    {
+        var tarFile = TarFile();
+
+        var properties = new Properties()
+        {
+            FileMode   = FileMode.Parse("644"),
+            GroupId = 0,
+            OwnerId = 0,
+            LastModification = DateTimeOffset.Now,
+            Length = tarFile.Bytes.ToEnumerable().Count(),
+        };
+
+        var entry = new EntryData("File.tar", properties, () => TarFile().Bytes);
+        await using var output = File.Create("C:\\Users\\JMN\\Desktop\\ArWithTarInside.ar");
+        await new ArFile(entry).Bytes.DumpTo(output);
+    }
+
+    public TarFile TarFile()
+    {
+        var entry1 = new Archiver.Tar.EntryData("recordatorioCita.pdf", new Archiver.Tar.Properties
+        {
+            FileMode = FileMode.Parse("644"),
+            GroupId = 1000,
+            GroupName = "jmn",
+            LastModification = DateTimeOffset.Now,
+            OwnerId = 1000,
+            OwnerUsername = "jmn",
+            Length = new FileInfo("D:\\5 - Unimportant\\Descargas\\recordatorioCita.pdf").Length
+        }, () => Observable.Using(() => File.OpenRead("D:\\5 - Unimportant\\Descargas\\recordatorioCita.pdf"), stream => stream.ToObservable()));
+
+        var entry2 = new Archiver.Tar.EntryData("wasabi.deb", new Archiver.Tar.Properties
+        {
+            FileMode = FileMode.Parse("644"),
+            GroupId = 1000,
+            GroupName = "jmn",
+            LastModification = DateTimeOffset.Now,
+            OwnerId = 1000,
+            OwnerUsername = "jmn",
+            Length = new FileInfo("D:\\5 - Unimportant\\Descargas\\Wasabi-2.0.4.deb").Length
+        }, () => Observable.Using(() => File.OpenRead("D:\\5 - Unimportant\\Descargas\\Wasabi-2.0.4.deb"), stream => stream.ToObservable()));
+
+        return new TarFile(entry1, entry2);
     }
 }
