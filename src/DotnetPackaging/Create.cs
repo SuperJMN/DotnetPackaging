aaa2c5d6756a9d562ffe05000b88596c28a46e6f
José Manuel Nieto <superjmn@outlook.com>
2023-11-06 22:11:01 +0100

    Make it easier Deb file creation (#3)
    * Easier deb building
    
    * Workaround for dump

---
+++ src/DotnetPackaging/Create.cs
@@ -0,0 +1,29 @@
+﻿using System.IO.Abstractions;
+using CSharpFunctionalExtensions;
+using DotnetPackaging.Deb;
+using Serilog;
+using Zafiro.FileSystem;
+using Zafiro.FileSystem.Local;
+
+namespace DotnetPackaging;
+
+public static class Create
+{
+    /// <summary>
+    /// Create a .deb file
+    /// </summary>
+    /// <param name="contentsPath">Folder where the application has been published. It should contain the files compiled for Linux</param>
+    /// <param name="outputPathForDebFile">Path of the output .deb file.</param>
+    /// <param name="metadata">Metadata of the .deb file.</param>
+    /// <param name="executableFiles">Mapping to identify which files are executable and their properties (Desktop entries and so).</param>
+    /// <returns>A Result to indicate whether the operation succeeded or not</returns>
+    public static async Task<Result> Deb(string contentsPath, string outputPathForDebFile, Metadata metadata, Dictionary<ZafiroPath, ExecutableMetadata> executableFiles)
+    {
+        var fs = new LocalFileSystem(new FileSystem(), Maybe<ILogger>.None);
+
+        return await
+            from contentDirectory in fs.GetDirectory(contentsPath.ToZafiroPath())
+            from output in fs.GetFile(outputPathForDebFile.ToZafiroPath())
+            select new DebBuilder().Create(contentDirectory, metadata, executableFiles, output);
+    }
+}
