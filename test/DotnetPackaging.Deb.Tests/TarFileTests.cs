a0f8bf580c12459f6b3b493924c929ddb4823466
José Manuel Nieto <superjmn@outlook.com>
2024-04-24 14:00:01 +0200

    Dale, dale

--- test/DotnetPackaging.Deb.Tests/TarFileTests.cs
+++ test/DotnetPackaging.Deb.Tests/TarFileTests.cs
@@ -1,5 +1,4 @@
-﻿using DotnetPackaging.Deb.Archives.Ar;
-using DotnetPackaging.Deb.Archives.Tar;
+﻿using DotnetPackaging.Deb.Archives.Tar;
 using FluentAssertions;
 using FluentAssertions.Extensions;
 using Xunit;
@@ -40,27 +39,6 @@ public class TarFileTests
         var outputStream = new MemoryStream();
         var result = await TarWriter.Write(tarFile, outputStream);
         result.Should().Succeed();
-        outputStream.Position = 0;
-        
-        await using (var fileStream = File.Open("C:\\Users\\JMN\\Desktop\\actual2.tar", FileMode.Create))
-        {
-            outputStream.WriteTo(fileStream);
-        }
-
         outputStream.Should().BeEquivalentTo(File.OpenRead("TestFiles/Sample.tar"));
-    }
-
-    private TarProperties DefaultProperties()
-    {
-        return new TarProperties()
-        {
-            FileMode = UnixFilePermissions.AllPermissions,
-            GroupId = 1,
-            GroupName = "group",
-            OwnerUsername = "owner",
-            LinkIndicator = 1,
-            OwnerId = 0,
-            LastModification = 20.January(2020),
-        };
     }
 }
