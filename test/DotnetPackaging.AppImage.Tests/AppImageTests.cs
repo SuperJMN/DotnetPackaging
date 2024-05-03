adc44dfcfcf69fcb327234d06323c5724470fd50
José Manuel Nieto <superjmn@outlook.com>
2024-05-04 00:10:21 +0200

    Builder

--- test/DotnetPackaging.AppImage.Tests/AppImageTests.cs
+++ test/DotnetPackaging.AppImage.Tests/AppImageTests.cs
@@ -1,4 +1,5 @@
 ﻿using Zafiro.FileSystem.Lightweight;
+using Directory = Zafiro.FileSystem.Lightweight.Directory;
 
 namespace DotnetPackaging.AppImage.Tests;
 
@@ -7,11 +8,81 @@ public class AppImageTests
     [Fact]
     public void Test()
     {
-        //var appImage = new AppImage(new FakeRuntime(), new Application()
-        //{
-        //    Metadata = new PackageMetadata(),
-        //    Contents = new Root();
-        //});
+        var builder = new DebFileBuilder()
+            .FromDirectory(new Directory("mama", new List<IFile>(), new List<IDirectory>()))
+            .Configure(setup => setup
+                .Package("AvaloniaSyncer")
+                .PackageId("com.SuperJMN.AvaloniaSyncer")
+                .ExecutableName("AvaloniaSyncer.Desktop"))
+            .Build();
+    }
+}
+
+public class DebFileBuilder
+{
+    public FromContainerOptions FromDirectory(IDirectory directory)
+    {
+        return new FromContainerOptions(directory);
+    }
+}
+
+public class ContainerOptionsSetup
+{
+    private string package;
+    private string packageId;
+    private string executableName;
+
+    public ContainerOptionsSetup Package(string package)
+    {
+        this.package = package;
+        return this;
+    }
+    
+    public ContainerOptionsSetup PackageId(string packageId)
+    {
+        this.packageId = packageId;
+        return this;
+
+    }
+
+    public ContainerOptionsSetup ExecutableName(string executableName)
+    {
+        this.executableName = executableName;
+        return this;
+    }
+}
+
+public class FromContainerOptions
+{
+    private readonly IDirectory container;
+
+    public FromContainerOptions(IDirectory container)
+    {
+        this.container = container;
+    }
+
+    public FromContainer Configure(Action<ContainerOptionsSetup> setup)
+    {
+        var options = new ContainerOptionsSetup();
+        setup(options);
+        return new FromContainer(container, options);
+    }
+}
+
+public class FromContainer
+{
+    private readonly IDirectory directory;
+    private readonly ContainerOptionsSetup setup;
+
+    public FromContainer(IDirectory directory, ContainerOptionsSetup setup)
+    {
+        this.directory = directory;
+        this.setup = setup;
+    }
+
+    public AppImage Build()
+    {
+        return new AppImage(new FakeRuntime(), new Application());
     }
 }
 
