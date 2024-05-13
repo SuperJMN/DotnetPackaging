6d5ec41cfbb3164edeef04b0f16411eb52c62fde
José Manuel Nieto <superjmn@outlook.com>
2024-05-13 19:19:30 +0200

    Delicado

---
+++ test/DotnetPackaging.AppImage.Tests/FromRootedTests.cs
@@ -0,0 +1,22 @@
+﻿using Zafiro.DataModel;
+using Zafiro.FileSystem.Unix;
+using File = Zafiro.FileSystem.Lightweight.File;
+
+namespace DotnetPackaging.AppImage.Tests;
+
+public class FromRootedTests
+{
+    [Fact]
+    public void FromRooted()
+    {
+        var files = new[]
+        {
+            new RootedUnixFile(ZafiroPath.Empty, new UnixFile("Sample1.txt", (StringData) "Content")),
+            new RootedUnixFile(ZafiroPath.Empty, new UnixFile("Sample2.txt", (StringData)"Content")),
+            new RootedUnixFile("Dir", new UnixFile("Sample3.txt", (StringData)"Content")),
+            new RootedUnixFile("Dir/Subdir", new UnixFile("Sample4.txt", (StringData)"Content")),
+        };
+
+        var root = files.FromRootedFiles(ZafiroPath.Empty);
+    }
+}
