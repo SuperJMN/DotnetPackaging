d816602462f9d5cbd1dc5cdabebdc2ff5fd76bb9
José Manuel Nieto <superjmn@outlook.com>
2024-06-10 11:48:07 +0200

    Update project build process and dependencies (#15)
    The project's build process was updated, including improvements to the publish target, with additional GitHub release and NuGet package publishing steps. The target framework was also upgraded from .net7.0 to .net8.0, and a dependency on the Zafiro.Nuke package was introduced. Changes were reflected in the azure-pipelines.yml as well.

--- build/Build.cs
+++ build/Build.cs
@@ -1,90 +1,130 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
+using DotnetPackaging;
 using Nuke.Common;
-using Nuke.Common.CI.AzurePipelines;
 using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.GitVersion;
 using Serilog;
+using Zafiro.Mixins;
+using Zafiro.Nuke;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
-using static Nuke.Common.Tooling.ProcessTasks;
-using Project = Nuke.Common.ProjectModel.Project;
+using Maybe = CSharpFunctionalExtensions.Maybe;
 
+namespace _build;
 
-[AzurePipelines(AzurePipelinesImage.WindowsLatest, ImportSecrets = new[] { nameof(NuGetApiKey) }, AutoGenerate = false)]
 class Build : NukeBuild
 {
-    [Parameter] [Secret] readonly string NuGetApiKey;
+    [Parameter("The alias for the key in the keystore.")] readonly string AndroidSigningKeyAlias;
+    [Parameter("The password of the key within the keystore file.")] [Secret] readonly string AndroidSigningKeyPass;
+    [Parameter("The password for the keystore file.")] [Secret] readonly string AndroidSigningStorePass;
+    [Parameter("Contents of the keystore encoded as Base64.")] readonly string Base64Keystore;
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")] readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-    [Solution] readonly Solution Solution;
-
-    [Parameter("version-suffix")] public string VersionSuffix { get; set; }
-
-    [Parameter("publish-framework")] public string PublishFramework { get; set; }
-
-    [Parameter("publish-runtime")] public string PublishRuntime { get; set; }
-
-    [Parameter("publish-project")] public string PublishProject { get; set; }
-
-    [Parameter("publish-self-contained")] public bool PublishSelfContained { get; set; } = true;
-
+    [Parameter("GitHub Authentication Token")] [Secret] readonly string GitHubAuthenticationToken;
+    [Parameter("NuGet Authentication Token")] [Secret] readonly string NuGetApiKey;
+    
     [GitVersion] readonly GitVersion GitVersion;
-
     [GitRepository] readonly GitRepository Repository;
 
-    [Parameter]
-    readonly Configuration Configuration = IsServerBuild
-        ? Configuration.Release
-        : Configuration.Debug;
+    public AbsolutePath OutputDirectory = RootDirectory / "output";
 
-    AbsolutePath OutputDirectory => RootDirectory / "output";
+    [Solution] Solution Solution;
+    
+    public static int Main() => Execute<Build>(x => x.Publish);
 
-    Target Clean => _ => _
+    protected override void OnBuildInitialized()
+    {
+        Actions = new Actions(Solution, Repository, RootDirectory, GitVersion, Configuration);
+    }
+
+    Actions Actions { get; set; }
+
+    Target Clean => d => d
         .Executes(() =>
         {
-            OutputDirectory.CreateOrCleanDirectory();
             var absolutePaths = RootDirectory.GlobDirectories("**/bin", "**/obj").Where(a => !((string) a).Contains("build")).ToList();
             Log.Information("Deleting {Dirs}", absolutePaths);
             absolutePaths.DeleteDirectories();
         });
 
-    Target Pack => _ => _
-        .DependsOn(Clean)
+    Target RestoreWorkloads => td => td
         .Executes(() =>
         {
-            var packableProjects = Solution.AllProjects.Where(x => x.GetProperty<bool>("IsPackable")).ToList();
-
-            packableProjects.ForEach(project =>
-            {
-                Log.Information("Restoring workloads of {Input}", project);
-                RestoreProjectWorkload(project);
-            });
-
-            DotNetPack(settings => settings
-                .SetConfiguration(Configuration)
-                .SetVersion(GitVersion.NuGetVersion)
-                .SetOutputDirectory(OutputDirectory)
-                .CombineWith(packableProjects, (packSettings, project) =>
-                    packSettings.SetProject(project)));
+            DotNetWorkloadRestore(x => x.SetProject(Solution));
         });
-
-    Target Publish => _ => _
-        .DependsOn(Pack)
+    
+    Target PublishNuGetPackages => d => d
         .Requires(() => NuGetApiKey)
-        .OnlyWhenStatic(() => Repository.IsOnMasterBranch())
+        .DependsOn(Clean)
+        .OnlyWhenStatic(() => Repository.IsOnMainOrMasterBranch())
         .Executes(() =>
         {
-            DotNetNuGetPush(settings => settings
-                    .SetSource("https://api.nuget.org/v3/index.json")
-                    .SetApiKey(NuGetApiKey)
-                    .CombineWith(
-                        OutputDirectory.GlobFiles("*.nupkg").NotEmpty(), (s, v) => s.SetTargetPath(v)),
-                degreeOfParallelism: 5, completeOnFailure: true);
+            var actions = new Actions(Solution, Repository, RootDirectory, GitVersion, Configuration);
+            actions.PushNuGetPackages(NuGetApiKey)
+                .TapError(error => throw new ApplicationException(error));
         });
 
-    public static int Main() => Execute<Build>(x => x.Publish);
+    Target PublishGitHubRelease => td => td
+        .DependsOn(RestoreWorkloads)
+        .OnlyWhenStatic(() => Repository.IsOnMainOrMasterBranch())
+        .Requires(() => GitHubAuthenticationToken)
+        .Executes(async () =>
+        {
+            var windowsFiles = Task.FromResult(Actions.CreateWindowsPacks());
+            var options = Options();
+            var linuxAppImageFiles = Actions.CreateLinuxAppImages(options);
+            
+            var allFiles = new[] { windowsFiles, linuxAppImageFiles }.Combine();
+            await allFiles
+                .Bind(paths => Actions.CreateGitHubRelease(GitHubAuthenticationToken, paths.Flatten().ToArray()))
+                .TapError(e => throw new ApplicationException(e));
+        });
 
-    void RestoreProjectWorkload(Project project) => StartShell($@"dotnet workload restore --project {project.Path}").AssertZeroExitCode();
+    Target Publish => td => td
+        .DependsOn(PublishGitHubRelease, PublishNuGetPackages);
+
+    Options Options()
+    {
+        IEnumerable<AdditionalCategory> additionalCategories = [
+            AdditionalCategory.Building, 
+            AdditionalCategory.Archiving,
+            AdditionalCategory.GTK,
+            AdditionalCategory.KDE,
+        ];
+
+        return new Options
+        {
+            MainCategory = MainCategory.Utility,
+            AdditionalCategories = Maybe.From(additionalCategories),
+            AppName = "DotnetPackagingGUI",
+            Version = GitVersion.MajorMinorPatch,
+            Comment = "Packager for .NET stand-alone applications, powered by AvaloniaUI",
+            AppId = "com.SuperJMN.DotnetPackaging.GUI",
+            StartupWmClass = "DotnetPackaging",
+            HomePage = new Uri("https://github.com/SuperJMN/dotnetpackaging"),
+            Keywords = new List<string>
+            {
+                "Installer",
+                "Cross-Platform",
+                "AvaloniaUI",
+                "Avalonia",
+                "AppImage",
+                ".deb",
+                ".appimage",
+                "Open Source",
+                "Distribute",
+                "Package",
+            },
+            License = "MIT",
+            ScreenshotUrls = Maybe<IEnumerable<Uri>>.None,
+            Summary = "Lets you packager your .NET application for distributing them easily. Currently supported formats are .deb and .appimage."
+        };
+    }
 }
