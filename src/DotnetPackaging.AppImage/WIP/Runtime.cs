2a8bbb5a21cb01307a2e72f0d5c5410c37739fb4
SuperJMN <superjmn@outlook.com>
2025-07-05 00:21:14 +0200

    Add Runtime and RuntimeFactory classes for AppImage support; refactor ByteSource methods

---
+++ src/DotnetPackaging.AppImage/WIP/Runtime.cs
@@ -0,0 +1,15 @@
+using Zafiro.DivineBytes;
+
+namespace DotnetPackaging.AppImage.Tests2;
+
+public class Runtime(IByteSource source) : IRuntime
+{
+    public IByteSource Source { get; } = source;
+
+    public IDisposable Subscribe(IObserver<byte[]> observer)
+    {
+        return Source.Subscribe(observer);
+    }
+
+    public IObservable<byte[]> Bytes => Source;
+}
---
+++ src/DotnetPackaging.AppImage/WIP/RuntimeFactory.cs
@@ -0,0 +1,23 @@
+using DotnetPackaging.AppImage.Tests2;
+using Zafiro.DivineBytes;
+
+namespace DotnetPackaging.AppImage.WIP;
+
+public static class RuntimeFactory
+{
+    private static readonly Dictionary<Architecture, Uri> RuntimeUrls = new()
+    {
+        { Architecture.X86, new ("https://github.com/AppImage/AppImageKit/releases/download/continuous/runtime-x86_64") },
+        { Architecture.X64, new ("https://github.com/AppImage/AppImageKit/releases/download/continuous/runtime-x86_64") },
+        { Architecture.Arm32, new("https://github.com/AppImage/AppImageKit/releases/download/continuous/runtime-armhf") },
+        { Architecture.Arm64, new("https://github.com/AppImage/AppImageKit/releases/download/continuous/runtime-aarch64") },
+    };
+
+    public static Task<Result<IRuntime>> Create(Architecture architecture)
+    {
+        return RuntimeUrls
+            .TryFind(architecture).ToResult($"Could not find architecture {architecture}")
+            .Bind(uri => uri.FromUri())
+            .Map(IRuntime (source) => new Runtime(source));
+    }
+}
