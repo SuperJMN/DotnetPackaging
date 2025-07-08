296fbf053da6765302f7a3f59d0b006faf81a3e3
SuperJMN <superjmn@outlook.com>
2025-07-08 12:53:13 +0200

    Enhance AppImage processing: add debugging assertions to verify byte array lengths and ensure data integrity during image concatenation

--- src/DotnetPackaging.AppImage/Core/AppImageExtensions.cs
+++ src/DotnetPackaging.AppImage/Core/AppImageExtensions.cs
@@ -5,11 +5,10 @@ namespace DotnetPackaging.AppImage.Core;
 
 public static class AppImageExtensions
 {
-    public static Task<Result<IByteSource>> ToByteSource(this AppImageContainer appImageContainer)
+    public static Result<IByteSource> ToByteSource(this AppImageContainer appImageContainer)
     {
-        return SquashFS.Create(appImageContainer.Container).Map(async sqfs =>
+        return SquashFS.Create(appImageContainer.Container).Map(sqfs =>
         {
-            await sqfs.WriteTo($"/home/jmn/Escritorio/AppImageContents-{appImageContainer.Runtime.Architecture}.squashfs");
             var appImageBytes = appImageContainer.Runtime.Concat(sqfs);
             return ByteSource.FromByteChunks(appImageBytes);
         });
