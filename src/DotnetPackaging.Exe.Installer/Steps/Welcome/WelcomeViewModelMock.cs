d7094d4c314ffa18b1550897cb82557323ff62da
SuperJMN <superjmn@outlook.com>
2025-11-15 22:34:26 +0100

    Refactor WelcomeViewModelMock and adjust binding in WelcomeView

--- src/DotnetPackaging.Exe.Installer/Steps/Welcome/WelcomeViewModelMock.cs
+++ src/DotnetPackaging.Exe.Installer/Steps/Welcome/WelcomeViewModelMock.cs
@@ -7,15 +7,16 @@ namespace DotnetPackaging.Exe.Installer.Steps.Welcome;
 
 public class WelcomeViewModelMock : IWelcomeViewModel
 {
-    private Reactive.Bindings.ReactiveProperty<InstallerMetadata?> metadata;
+    public WelcomeViewModelMock()
+    {
+        Metadata = new Reactive.Bindings.ReactiveProperty<InstallerMetadata>(new InstallerMetadata(
+            "com.example.app",
+            "Example App",
+            "1.0.0",
+            "Example, Inc.", Description: "This is an example app. It does nothing. It's just a demo."));
+    }
 
-    public InstallerMetadata Metadata { get; } = new InstallerMetadata(
-        "com.example.app",
-        "Example App",
-        "1.0.0",
-        "Example, Inc.", Description: "");
+    public Reactive.Bindings.ReactiveProperty<InstallerMetadata?> Metadata { get; }
 
     public ReactiveCommand<Unit, Result<InstallerMetadata>> LoadMetadata { get; }
-
-    Reactive.Bindings.ReactiveProperty<InstallerMetadata?> IWelcomeViewModel.Metadata => metadata;
 }
