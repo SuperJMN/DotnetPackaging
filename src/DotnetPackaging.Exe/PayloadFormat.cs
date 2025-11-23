97ab4320cbd98d2491cc4b349c311b5883e8442b
José Manuel Nieto <superjmn@outlook.com>
2025-11-23 01:05:05 +0100

    Remove ResourceExePacker and PayloadFormat from the project

--- src/DotnetPackaging.Exe/PayloadFormat.cs
+++
@@ -1,7 +0,0 @@
-namespace DotnetPackaging.Exe;
-
-internal static class PayloadFormat
-{
-    public const string Magic = "DPACKEXE1"; // 9 ASCII bytes
-    public const int FooterLength = 8 /*len*/ + 9 /*magic*/; // 17 bytes
-}
